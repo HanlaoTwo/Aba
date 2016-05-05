c85c92b90f32554d209ea82a304c200f37a281cd
CodingDancer <hankeboom@163.com>
2016-05-05 16:59:47 +0800

    跟新防止修改错误导致崩溃

--- WebContent/JSPS/information.jsp
+++ WebContent/JSPS/information.jsp
@@ -1,39 +1,48 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="../CSS/bootstrap.css" />
-
     <title>服务消息</title>
 </head>
 <body>
-
 <div class="container">
     <h3 class="h3">已经选择的区域：${section}</h3>
-    <h3 class="h3">已经选择的服务类型：${service}</h3>
-
     <table class="table table-hover">
-
-
         <tr>
-            <td class="active">欢迎短信</td>
-            <td class="success">目的地介绍</td>
-            <td class="warning">景区天气</td>
-            <td class="danger">客流实时消息</td>
+            <td >欢迎短信</td>
+            <td class="active">${message}</td>
         </tr>
         <tr>
-            <td class="active">${message}</td>
+             <td >目的地介绍</td>
             <td class="success">${description}</td>
+        </tr>
+        <tr>           
+            <td >景区天气</td>
             <td class="warning">${weather}</td>
+        </tr>
+         <tr>
+            <td >客流实时消息</td>
             <td class="danger">${info}</td>
         </tr>
+         <tr>
+            <td >商业消息</td>
+            <td class="success">${tration}</td>
+        </tr>
+         <tr>
+            <td >应急消息</td>
+            <td class="active">${emerg}</td>
+        </tr>
     </table>
+    <a  href="http://localhost:8080/Aba/JSPS/user.html"><button class="btn-info">消息推送</button></a>
+    <script style="text/javascript">
+        function MSGpush()
+        {
 
-
+        }
+    </script>
     </div>
-
 </body>
 </html>
--- src/com/aba/controller/hq/infoController.java
+++ src/com/aba/controller/hq/infoController.java
@@ -1,8 +1,11 @@
 package com.aba.controller.hq;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.xml.crypto.Data;
+
+import model.Message;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -17,7 +20,7 @@ import dao.sectiondao;
 
 
 @Controller
-@RequestMapping(value = "/JSPS",method = RequestMethod.GET)
+@RequestMapping(value = "/JSPS")
 public class infoController {
 	@RequestMapping(value = "/service")
 	public String choseSection(ModelMap map,@RequestParam String section){
@@ -27,28 +30,49 @@ public class infoController {
 	
 	@RequestMapping(value = "/information")
 	public String getMessage(ModelMap map,
-			@RequestParam int section,
-			@RequestParam int service)
+			@RequestParam int section)
 	{
 		sectiondao sd = new sectiondao();
 		Messagedao md = new Messagedao();
+		Message m = md.getMessageByID(section).get(0);
 		map.addAttribute("section",section);
-		map.addAttribute("service",service);
-		System.out.println("diqu:"+section+"  "+"fuwu: "+service);
-		map.addAttribute("info", "ÔÝÎÞ");
+		map.addAttribute("info", "ï¿½ï¿½ï¿½ï¿½");
 		map.addAttribute("message", sd.getSection(section).getMsg());
 		map.addAttribute("description", sd.getSection(section).getDescription());
-		//map.addAttribute("weather", md.getMessage(4).getWeather());
-		map.addAttribute("weather", md.getMessageByID(section).get(0).getWeather());
-		
+		map.addAttribute("weather", m.getWeather());
+		map.addAttribute("tration", m.getTration());
+		map.addAttribute("emerg", m.getEmergency());
 		return "/information";	
 	}
-	//Ä£ÄâÊý¾Ý
+	@RequestMapping(value="/msgpush")
+	public String setMessage(@RequestParam String weather, 
+			                 @RequestParam String tration,
+			                 @RequestParam String emergency,
+			                 @RequestParam int sectionid)
+	{
+		System.out.println("controller>>>>>>>>>>>>>>>>"+emergency);
+		Messagedao md = new Messagedao();
+		Date time_data = new Date();
+		md.saveMessage(weather, time_data, tration, emergency, sectionid);
+		
+		return "/setmsg";
+	}
+	
+	@RequestMapping(value="/setsection")
+	public String setSection(@RequestParam int sectionid, 
+                             @RequestParam String msg,
+                             @RequestParam String descrption)
+	{
+		sectiondao sd = new sectiondao();
+		sd.saveSection(sectionid, msg, descrption);
+		return "/setmsg";
+	}
+	//Ä£ï¿½ï¿½ï¿½ï¿½ï¿½
 	@RequestMapping(value = "/datasimulate")
 	public String getMessage(@RequestParam int[] num,
 			                 @RequestParam String[] propotion,
 			                 @RequestParam String time){
-		//Ä£ÄâÊäÈë
+		//Ä£ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 		Simulater simulater = new Simulater();
 		simulater.getParm(num, propotion, time);
 		return "/dataOK";
